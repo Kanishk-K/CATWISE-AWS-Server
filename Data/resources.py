e76de2897f7c406d3b6dda766e551c03b289ed0c
Kanishk <kanishkkacholia@gmail.com>
2021-04-12 23:43:38 -0500

    Removed exclusion filters since it was excluding in exports too.

--- Data/resources.py
+++ Data/resources.py
@@ -5,18 +5,6 @@ class CatWiseResource(resources.ModelResource):
 
     class Meta:
         model = CatWise
-        exclude = (
-            'VTan',
-            'W1W2Diff',
-            'W1W2DiffErr',
-            'TotalPM',
-            'TotalPMErr',
-            'H_W2',
-            'JW2Diff',
-            'JW2DiffErr',
-            'WiseURL',
-            'created_at',
-        )
         import_id_fields=[
             'id'
         ]
